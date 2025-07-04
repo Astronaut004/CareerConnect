9e1db749dda59d45731a63dd50d57d02bedadd3d
Astronaut004 <Astronaut004@users.noreply.github.com>
2025-07-04 17:36:17 +0530

    add middleware to protect from unwanted traffic

---
+++ server_backend/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+const protect = (req,res,next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if(!token) return res.status(401).json({error: 'Not authorized, no token'});
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId;
+    next();
+  }
+  catch (error) {
+    return res.status(401).json({error: 'Not authorized, token failed'});
+  }
+}
+
+export default protect;
